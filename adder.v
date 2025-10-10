48f793fe3668f0f8cdca2b8cfcf5aeb9722fd54b
PaigeClarkson <paigeclarkson@u.boisestate.edu>
2025-10-10 10:45:31 -0600

    Update adder.v

--- adder.v
+++ adder.v
@@ -1,9 +1,17 @@
 module adder(
     // Declare your A/B inputs
+    input A,       
+    input B,
+
     // Declare Y output
+    output Y,      
+
     // Declare carry output
+    output Cout
 );
 
     // Enter logic equation here
+    assign Y = A ^ B;     
+    assign Cout = A & B;
 
 endmodule
